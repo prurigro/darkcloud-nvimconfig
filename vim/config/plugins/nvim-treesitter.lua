221712b26a9df5db1bedb70353da7ba6e7821332
Kevin MacMartin <prurigro@gmail.com>
2024-03-05 13:02:49 -0500

    Syntax tweak in nvim-treesitter

--- vim/config/plugins/nvim-treesitter.lua
+++ vim/config/plugins/nvim-treesitter.lua
@@ -9,7 +9,7 @@ if (vim.g.enabletreesitter == 1) then
 
     vim.opt.runtimepath:append(parser_dir)
 
-    require'nvim-treesitter.configs'.setup {
+    require("nvim-treesitter.configs").setup {
         ensure_installed = { "bash", "c", "cpp", "git_config", "html", "java", "javascript", "json", "lua", "php", "python", "query", "rust", "toml", "typescript", "vim", "vue" },
         sync_install = false,
         auto_install = false,
