b78b285fb1a190fb4e311bac9f95419619dfefdb
Kevin MacMartin <prurigro@gmail.com>
2024-04-02 17:58:02 -0400

    Disable treesitter for tmux

--- vim/config/plugins/nvim-treesitter.lua
+++ vim/config/plugins/nvim-treesitter.lua
@@ -17,7 +17,7 @@ if (vim.g.enabletreesitter == 1) then
 
         highlight = {
             enable = true,
-            disable = { "gitcommit", "markdown" },
+            disable = { "gitcommit", "markdown", "tmux" },
             additional_vim_regex_highlighting = { "diff", "scss" },
         },
 
